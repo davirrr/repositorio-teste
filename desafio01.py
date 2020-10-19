1afc480eaf87c27aed33b1b993e37aef532db101
davirrr <davibarke182@gmail.com>
2020-10-19 15:33:07 -0300

    Aff, de novo

---
+++ desafio01.py
@@ -0,0 +1,2 @@
+nome = input('Qual o seu nome?')
+print('Olá, querido', nome, '! Seja bem-vindo!')
---
+++ desafio02.py
@@ -0,0 +1,4 @@
+dia = input('Em qual dia você nasceu?: ')
+mês = input('Ok. E o mês?: ')
+ano = input('Maravilha! por último, o ano do seu nascimento: ')
+print('Então a sua data de nascimento é',dia,'de',mês,'de',ano,'. Acertei?')
